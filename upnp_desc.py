1fdcd3e0636d1c9b1951bdb41497f011b68dcf17
scpcom <scpcom@gmx.de>
2021-04-01 20:18:21 +0200

    try upnp_desc with both upnp_ns

--- upnp_desc.py
+++ upnp_desc.py
@@ -9,8 +9,14 @@ if len(sys.argv) > 1:
     except Exception as e:
         print('parse failed: {0}'.format(e))
         device = None
+    if device is None:
+        set_upnp_ns(1)
+        device = upnp_process_description(sys.argv[1])
     if device is not None:
         upnp_print_schema(device)
         print('Vendor: '+device.manufacturer)
         print('Model: '+device.model_name)
-        print('Description: '+device.model_description)
+        if device.model_description:
+            print('Description: '+device.model_description)
+        if device.model_number:
+            print('Number: '+device.model_number)
